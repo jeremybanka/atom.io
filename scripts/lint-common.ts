792c286100acee75d6365dd1abe3a084659b1aee
Jeremy Banka <hello@jeremybanka.com>
2026-06-17 12:26:15 -0700

    ♻️ factor out lint ignore list (#116)

---
+++ scripts/lint-common.ts
@@ -0,0 +1,10 @@
+export const LINT_IGNORES: string[] = [
+	`**/.astro/**`,
+	`**/.wrangler/**`,
+	`**/build/**`,
+	`**/coverage/**`,
+	`**/dist/**`,
+	`**/gen/**`,
+	`**/node_modules/**`,
+	`**/storybook-static/**`,
+]

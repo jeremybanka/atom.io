a37df0c8ac20ef545aa4aa2a41db19fbe38d876d
Jeremy Banka <hello@jeremybanka.com>
2026-06-19 17:33:50 -0700

    docs: document transaction toolkit

---
+++ packages/atom.io/docs/source/exhibits/core/transaction/dispose-a-family-member-in-a-transaction.ts
@@ -0,0 +1,30 @@
+import { atom, atomFamily, transaction } from "atom.io"
+
+type Draft = {
+	id: string
+	title: string
+}
+
+export const draftAtoms = atomFamily<Draft, string>({
+	key: `draft`,
+	default: (id) => ({ id, title: `` }),
+})
+
+export const draftKeysAtom = atom<string[]>({
+	key: `draftKeys`,
+	default: [],
+})
+
+export const deleteDraftTX = transaction<(draftId: string) => void>({
+	key: `deleteDraft`,
+	do: ({ dispose, get, set }, draftId) => {
+		const draftIds = get(draftKeysAtom)
+		if (!draftIds.includes(draftId)) return
+
+		set(
+			draftKeysAtom,
+			draftIds.filter((id) => id !== draftId),
+		)
+		dispose(draftAtoms, draftId)
+	},
+})
